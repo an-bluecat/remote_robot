ecd8e56db539e05634ad518e95d187ad2d8794d6
Kevin Liu <liuxiaoyang000514@gmail.com>
2023-02-12 22:22:41 -0500

    Code Cleanup
    Get rid of Propeller Board related content.
    Add .gitignore to filter out changes in cache files

--- PropellerBoard/LegoRover/lights.h
+++
@@ -1,7 +0,0 @@
-#ifndef LIGHTS_H_INCLUDED
-#define LIGHTS_H_INCLUDED
-
-/* Loop for lights control */
-void lights_loop(void *ptr);
-
-#endif
--- PropellerBoard/MiniCar/motor.h
+++
@@ -1,7 +0,0 @@
-#ifndef MOTOR_H_INCLUDED
-#define MOTOR_H_INCLUDED
-
-/* Loop for lights control */
-void motor_loop(void *ptr);
-
-#endif
